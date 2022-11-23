73ad3afc67c8629747e1191e6d5c1c6a6a425524
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-23 15:45:36 +0200

    feat(26): object task

--- JavaScript essentials [24-49]/26 - arrays and objects/main.js
+++ JavaScript essentials [24-49]/26 - arrays and objects/main.js
@@ -1,1 +1,70 @@
+const obj = {
+  y: ' ',
+  a: {
+    g: 'proga',
+    p: 'Lietuva',
+    l: ['Kėglis']
+  },
+  b: 'Labas',
+  c: [
+    'Ponas',
+    'mėgstamiausia',
+    {
+      k: '.',
+      t: '!'
+    }
+  ],
+  e: {
+    d: 'žalia',
+    l: 'šalis',
+    a: [
+      {
+        a: 'geras',
+        v: 'galbūt',
+        p: ['susitikti']
+      }
+    ]
+  },
+  f: [
+    { a: ['šiandien', 'gera', 'mano', 'visai'] },
+    {
+      l: 'spalva',
+      a: {
+        s: 'kurioje',
+        u: 'oras',
+        o: ['?']
+      },
+      s: 'gyvenu'
+    }
+  ],
+  l: 'yra'
+}
+
+console.group('Lietuva yra šalis kurioje gyvenu.');
+{
+  const result = obj.a.p + obj.y + obj.l;
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('mano mėgstamiausia spalva yra žalia.');
+{
+  const result = '';
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('šiandien visai geras oras!');
+{
+  const result = '';
+  console.log(result);
+}
+console.groupEnd();
+
+console.group('galbūt gera proga susitikti?');
+{
+  const result = '';
+  console.log(result);
+}
+console.groupEnd();
 
