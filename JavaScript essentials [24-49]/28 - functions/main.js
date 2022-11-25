b9579c286eb30696cfe87487a686ef776b636b52
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-25 13:06:43 +0200

    init(28): functions

---
+++ JavaScript essentials [24-49]/28 - functions/main.js
@@ -0,0 +1,14 @@
+function atlktiMokejima(piniguSuma, saskaita, gavejoVardas) {
+  console.log('Siunčiama užklausa į banką...');
+  console.table({ piniguSuma, saskaita, gavejoVardas })
+}
+
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
+atlktiMokejima(50, 'LT354849756558454', 'Jonavis Grūdėla');
