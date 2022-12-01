a6bc9d77962f60f430ab8420dd497fa9f108f14a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-01 14:56:19 +0200

    feat(32): map tasks solved

--- JavaScript essentials [24-49]/32 - array iterative methods 2/tasks/map-tasks.js
+++ JavaScript essentials [24-49]/32 - array iterative methods 2/tasks/map-tasks.js
@@ -59,18 +59,33 @@ const numbers = [1, 15, 9, -9, -8, -7, 2, 8, 51, 6, 5];
 // Kiekvienai užduočiai padeklaruokite funkciją, kurią panaudosite kviesdami metodą Array.prototype.map
 console.group('1. Sukurkite funkciją kuri performuotų asmens objektą į vardo ir pavardės string`ą. Panaudokite Array.prototype.map, kad sukurti naują masyvą panaudojant šią funkciją.');
 {
+  function person2Fullname(person) {
+    return `${person.name} ${person.surname}`;
+  }
 
+  const fullnames = people.map(person2Fullname);
+  console.table(fullnames);
 }
 console.groupEnd();
 
 console.group('2. Sukurkite funkciją kuri performuotų asmens objektą į amžių. Panaudokite Array.prototype.map, kad sukurti naują masyvą panaudojant šią funkciją.');
 {
+  function person2Age(person) {
+    return person.age;
+  }
 
+  const ages = people.map(person2Age);
+  console.table(ages);
 }
 console.groupEnd();
 
 console.group('3. Sukurkite funkciją kuri padalintų skaičių iš 4. Panaudokite Array.prototype.map, kad sukurti naują masyvą panaudojant šią funkciją.');
 {
+  function divideBy4(number) {
+    return number / 4;
+  }
 
+  const quarters = numbers.map(divideBy4);
+  console.table(quarters);
 }
 console.groupEnd();
