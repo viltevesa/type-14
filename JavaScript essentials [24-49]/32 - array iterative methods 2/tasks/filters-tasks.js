73a93452a56afc44428a1e82d0b560b6b17b59f3
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-01 13:06:02 +0200

    init(32): array iterative methods 2

---
+++ JavaScript essentials [24-49]/32 - array iterative methods 2/tasks/filters-tasks.js
@@ -0,0 +1,77 @@
+const people = [
+  {
+    name: 'Jonas',
+    surname: 'Jonaitis',
+    sex: 'male',
+    age: 26,
+    income: 1200,
+    married: false,
+    hasCar: false
+  },
+  {
+    name: 'Severija',
+    surname: 'Piktutytė',
+    sex: 'female',
+    age: 26,
+    income: 1300,
+    married: false,
+    hasCar: true
+  },
+  {
+    name: 'Valdas',
+    surname: 'Vilktorinas',
+    sex: 'male',
+    age: 16,
+    income: 0,
+    married: false,
+    hasCar: false
+  },
+  {
+    name: 'Virginijus',
+    surname: 'Uostauskas',
+    sex: 'male',
+    age: 32,
+    income: 2400,
+    married: true,
+    hasCar: true
+  },
+  {
+    name: 'Samanta',
+    surname: 'Uostauskienė',
+    sex: 'female',
+    age: 28,
+    income: 1200,
+    married: true,
+    hasCar: true
+  },
+  {
+    name: 'Janina',
+    surname: 'Stalautinskienė',
+    sex: 'female',
+    age: 72,
+    income: 364,
+    married: false,
+    hasCar: false
+  }
+];
+const numbers = [1, 15, 9, -9, -8, -7, 2, 8, 51, 6, 5];
+
+console.group('1. Atrinkti neigiamus skaičius');
+{
+}
+console.groupEnd();
+
+console.group('2. Atrinkti nelyginius skaičius');
+{
+}
+console.groupEnd();
+
+console.group('3. Atrinkti žmones kurių vardas ilgesnis nei 6 raidės');
+{
+}
+console.groupEnd();
+
+console.group('4. Atrinkti žmones kurie turi mašiną');
+{
+}
+console.groupEnd();
---
+++ JavaScript essentials [24-49]/32 - array iterative methods 2/tasks/forEach-tasks.js
@@ -0,0 +1,64 @@
+const flats = [
+  {
+    address: 'Vilnakiemio g. 7',
+    rooms: [
+      { title: 'virtuvė', area: 12 },
+      { title: 'svetainė', area: 20 },
+      { title: 'tualetas', area: 8 },
+      { title: 'miegamasis', area: 16 },
+    ],
+    city: 'Vilnius',
+    price: 120000
+  },
+
+  {
+    address: 'Ruginių pr. 17',
+    rooms: [
+      { title: 'virtuvė', area: 18 },
+      { title: 'svetainė', area: 18 },
+      { title: 'tualetas', area: 8 },
+      { title: 'miegamasis', area: 12 },
+      { title: 'miegamasis2', area: 16 },
+    ],
+    city: 'Kaunas',
+    price: 120000
+  },
+
+  {
+    address: 'Saulės g. 7',
+    rooms: [
+      { title: 'virtuvė', area: 12 },
+      { title: 'svetainė', area: 20 },
+      { title: 'tualetas', area: 8 },
+    ],
+    city: 'Klaipėda',
+    price: 80000
+  },
+];
+
+// Kiekvienai užduočiai padeklaruokite funkciją, kurią panaudosite kviestami metodą Array.prototype.forEach
+
+// Jeigu reikia kaupti/formuoti reikšmę su kiekvienu masyvo elementu, naudojama metodologija:
+// 1. padeklaruojamas pradinis elementas
+// 2. Pradedamas ciklas
+//  2.1 Kiekviena iteracija papildo/keičia padeklaruotą kintamajį [1.]
+// 3. atliekamas galutinis formavimas
+// 4. grąžinama/spausdinama reikšmė 
+
+console.group('1. Atspausdinkite kiekvieno buto adresą su miestu');
+{
+
+}
+console.groupEnd();
+
+console.group('2. Atspausdinkite kiekvieno buto kambarių skaičių');
+{
+
+}
+console.groupEnd();
+
+console.group('3. Atspausdinkite kiekvieno buto bendrą plotą');
+{
+
+}
+console.groupEnd();
