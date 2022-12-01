374c197368d9d2e7026616be12632fdc1dcb5adc
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-01 16:32:23 +0200

    feat(32): reduce examples

--- JavaScript essentials [24-49]/32 - array iterative methods 2/main.js
+++ JavaScript essentials [24-49]/32 - array iterative methods 2/main.js
@@ -1,8 +1,31 @@
-function isEven(number) {
-  return number % 2 === 0;
+function sumReducer(prevSum, number) {
+  return prevSum + number;
+}
+const numbers = [7, 8, 1, 5, 4, 6, 1, 5, 8, 9];
+/*
+  Jeigu neperduodate pradinės reikšmės reduce metodui, tuomet pirmasis
+  masyvo elementas traktuojamas kaip pradinė reikšmė, o pradedama iteruoti nuo
+  antrojo masyvo elemento.
+*/
+// const sum = numbers.reduce(sumReducer);
+const sum = numbers.reduce(sumReducer, 0);
+
+console.log({ sum });
+
+// -------------------------------------------
+
+function drinkPriceReducer(prevTotal, drink) {
+  console.log({ prevTotal, drink });
+  return prevTotal + drink.price;
 }
 
-const numbers = [1, 2, 3, 4, 5, 777, 7, 8, 9, 10];
+const drinks = [
+  { name: "lemonade", price: 50 },
+  { name: "lime", price: 5 },
+  { name: "fanta", price: 40 },
+  { name: "cola", price: 20 },
+  { name: "pepsi", price: 30 },
+];
 
-const evenNumbers = numbers.filter(isEven);
-console.log(evenNumbers);
+const avgDrinkPrice = drinks.reduce(drinkPriceReducer, 0) / drinks.length;
+console.log(avgDrinkPrice);
