290ef59c9bb4ea4f9b9a45ee6ca5282862da6625
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-02 13:27:33 +0200

    feat(33): complex reduce example

--- JavaScript essentials [24-49]/33 - annonymous functions, lambda and destructurization/complex-reduce-example.js
+++ JavaScript essentials [24-49]/33 - annonymous functions, lambda and destructurization/complex-reduce-example.js
@@ -1,3 +1,41 @@
+const flats = [
+  {
+    address: 'Vilnakiemio g. 7',
+    rooms: [
+      { title: 'virtuvė', area: 12 },
+      { title: 'svetainė', area: 20 },
+      { title: 'tualetas', area: 8 },
+      { title: 'miegamasis', area: 16 },
+    ],
+    city: 'Vilnius',
+    price: 120000
+  },
+
+  {
+    address: 'Ruginių pr. 17',
+    rooms: [
+      { title: 'virtuvė', area: 18 },
+      { title: 'svetainė', area: 18 },
+      { title: 'tualetas', area: 8 },
+      { title: 'miegamasis', area: 12 },
+      { title: 'miegamasis2', area: 16 },
+    ],
+    city: 'Kaunas',
+    price: 120000
+  },
+
+  {
+    address: 'Saulės g. 7',
+    rooms: [
+      { title: 'virtuvė', area: 12 },
+      { title: 'svetainė', area: 20 },
+      { title: 'tualetas', area: 8 },
+    ],
+    city: 'Klaipėda',
+    price: 80000
+  },
+];
+
 console.group('7. Suskaičiuokite visų butų bendrą plotus sugrupuotus pagal miestą');
 {
   function roomAreaReducer(prevTotalArea, room) {
