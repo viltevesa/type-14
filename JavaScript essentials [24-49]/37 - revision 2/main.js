b38eec6e12a5083e570a4906af17dd70adc72394
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-08 14:41:00 +0200

    feat(37): forEach illiustration task

--- JavaScript essentials [24-49]/37 - revision 2/main.js
+++ JavaScript essentials [24-49]/37 - revision 2/main.js
@@ -1,13 +1,28 @@
-const fakeArray = {
+const numberArrayDemo = {
   0: 5,
   1: 6,
   2: 7,
   3: 8,
   4: 9,
+  5: 10,
+  length: 6,
+};
+
+/*
+  forEach yra 'Array' iteracinis metodas. Jo tikslas, tiesiog įvykdytį argumentu perduotą funkciją,
+  su kiekvienu masyvo elementu. Kviečiant argumentu perduotą funkciją, tai funkcijai yra siunčiami 
+  3 argumentai, tokia tvarka:
+    1. einamasis masyvo elementas,
+    2. einamojo masyvo elemento indeksas
+    3. masyvas per kurį yra iteruojama
+*/
+
+numberArrayDemo.__proto__.forEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
 }
 
-fakeArray.__proto__.printThis = function () {
-  console.log(this);
-}
+const printElement = (number) => console.log(number);
 
-fakeArray.printThis();
+numberArrayDemo.forEach(printElement);
