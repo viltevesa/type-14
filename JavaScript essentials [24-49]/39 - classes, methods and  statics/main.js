02f146af5e33d2196edccce04f76253c2225d23e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-12 15:29:43 +0200

    feat(39): instance vs object example

--- JavaScript essentials [24-49]/39 - classes, methods and  statics/main.js
+++ JavaScript essentials [24-49]/39 - classes, methods and  statics/main.js
@@ -35,39 +35,10 @@ class Automobilis {
 }
 
 const car1 = new Automobilis('Opel', 'Astra', 200);
-const car2 = new Automobilis('Opel', 'Astra', 200);
-const car3 = new Automobilis('Opel', 'Astra', 200);
+console.dir(Automobilis);
+console.dir(car1)
 
-console.log({ car1, car2, car3 });
+console.log(Automobilis.prototype);
+console.log(car1.__proto__);
 
-console.log(car1.marke);
-console.log(car2.marke);
-console.log(car3.marke);
-
-console.log(car1.modelis)
-console.log(car2.modelis)
-console.log(car3.modelis)
-
-console.log(car1.kaina);
-console.log(car2.kaina);
-console.log(car3.kaina);
-
-// 5. Padidinkite Automobilis objektų kainas šimtu.
-
-car1.price += 100;
-car2.price += 100;
-car3.price += 100;
-
-// 6. Po padidinimo, vėl atsispausdinkite objektus ir pažiūrėkite pakitusias Automobilis objektų savybes.
-
-console.log({ car1, car2, car3 });
-
-car1.užkurtiVariklį();
-car1.spaustPerTapkę(50);
-console.log(car1.greitis);
-car1.spaustPerTapkę(20);
-car1.mažintiGreitį(15);
-
-
-console.log(car1.greitis);
-
+console.log(car1.__proto__ === Automobilis.prototype);
