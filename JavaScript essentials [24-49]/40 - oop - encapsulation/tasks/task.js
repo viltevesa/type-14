1f0b473f00735b026d7e30b2b42d52279970616c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-13 13:05:41 +0200

    feat(40): oop - encapsulation

---
+++ JavaScript essentials [24-49]/40 - oop - encapsulation/tasks/task.js
@@ -0,0 +1,28 @@
+class House {
+
+}
+
+const houses = [
+  new House('Kimano g. 7', 'Kaunas', [{ name: 'Blakas Kojinskas' }]),
+  new House('Aliejų g. 121', 'Kaunas', [{ name: 'Sensas Jautimantas' }, { name: 'Rekė Jautimantė' }]),
+  new House('Sustenų g. 7', 'Vilnius', [{ name: 'Groja Blanienė' }, { name: 'Fartas Blanys' }]),
+  new House('Varkatkų g. 7', 'Klaipėda', [{ name: 'Faryja Mirgaitė' }]),
+];
+
+console.groupCollapsed('1. HouseInstance.owners savybės inkapsuliacija');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('2. Perrašykite HouseInstance.address savybės inkapsuliaciją naudojant naują sintaksę');
+{
+
+}
+console.groupEnd();
+
+console.groupCollapsed('3. Perrašykite HouseInstance.city savybės inkapsuliaciją naudojant naują sintaksę');
+{
+
+}
+console.groupEnd();
